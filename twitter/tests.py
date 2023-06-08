40ff732e51bec93aacdcbbc9db91e3cb9226ad82
KVijay2 <vijaykarunanidhi4@gmail.com>
2023-06-08 20:34:03 +0530

    follows page

--- twitter/tests.py
+++ twitter/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Tweet, Profile
 
+class URLTests(TestCase):
+  
+  def test_homepage(self):
+    response = self.client.get("/")
+    self.assertEqual(response.status_code,200)
 
+class ModelTests(TestCase):
+  
+  def profile_test_str(self):
+    user = Profile.objects.create(user="vijaykarunanidhi@gmail.com")
+    self.assertEqual(str(user),"vijaykarunanidhi@gmail.com")
