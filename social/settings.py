78221d07c8df1a3cba48d90f17114ee78e8e862a
K.Vijay <96011519+KVijay2@users.noreply.github.com>
2023-12-31 22:08:44 +0530

    Update settings.py

--- social/settings.py
+++ social/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d!))kdek=vr^h_=1p6+%$zfd&d1qgva(mjawk^yh$t9_1^vc=e'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','localhost:8000']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
